991c341648cd20fe5c8b9c699f4bccd1d3c57617
yutao <593156599@qq.com>
2024-11-30 06:24:48 +0800

    fix: 打包修改

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import pinia from './stores'
-import { setupAntd } from './plugins/antd'
-import { useUserStore } from '@/stores/user'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import pinia from "./stores";
+import { setupAntd } from "./plugins/antd";
+import { useUserStore } from "@/stores/user";
 
 // 引入样式
-import 'ant-design-vue/dist/reset.css'
-import './styles/index.less'
-import 'uno.css'
+import "ant-design-vue/dist/reset.css";
+import "./styles/index.less";
+import "uno.css";
 
-const app = createApp(App)
+const app = createApp(App);
 
 // 注册插件
-setupAntd(app)
-app.use(router)
-app.use(pinia)
+setupAntd(app);
+app.use(router);
+app.use(pinia);
 
 // 初始化用户状态
-const userStore = useUserStore()
-await userStore.initState()
+const userStore = useUserStore();
+userStore.initState();
 
-app.mount('#app')
+app.mount("#app");
